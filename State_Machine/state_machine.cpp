7d68b3f94a5ec6085f5baf72088240fa564277f8
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-07-25 10:23:23 +0200

    added while loop and exit condition

--- State_Machine/state_machine.cpp
+++ State_Machine/state_machine.cpp
@@ -17,7 +17,8 @@ int main(){
     cout << "Press 0 if you want your character to stand" << "\n"
          << "Press 1 if you want your character to walk" << "\n"
          << "Press 2 if you want your character to run" << "\n"
-         << "Press 3 if you want your character to stand" << "\n";
+         << "Press 3 if you want your character to crawl" << "\n"
+         << "Press 7 to finish" << "/n";
     cout <<"\n\n";
     
     enum m_s {stand, walk, run, crawl};
@@ -26,25 +27,32 @@ int main(){
     cout << "Your choice: " << "\n";
     cin >> choice;
     m_s state = (m_s)choice;
-    
-    switch(state){
-        case 0: cout << "You are standing. Next you can walk (1) or crawl (3)\n";
+
+    while(choice != 7){
+        switch(state){
+        case 0: cout << "You are standing. Next you can walk (1) or crawl (3). Press 7 to finish\n";
                 cout << "Your choice: " << "\n";
                 cin >> choice;
-                m_s state = (m_s)choice;
+                state = (m_s)choice;
                 break;
-        case 1: cout << "You are walking. Next you can stand (0) or run (2)\n";
+        case 1: cout << "You are walking. Next you can stand (0) or run (2). Press 7 to finish\n";
                 cout << "Your choice: " << "\n";
                 cin >> choice;
-                m_s state = (m_s)choice;
+                state = (m_s)choice;
                 break;
-        case 2: cout << "You are running. Next you can walk (1)\n";
-                m_s state = (m_s)1;
+        case 2: cout << "You are running. Next you can walk (1). Press 7 to finish\n";
+                cout << "Your choice: " << "\n";
+                cin >> choice;
+                state = (m_s)1;
                 break;
-        default:cout << "You are crawling. Next you can stand (0)\n";
-                m_s state = (m_s)0;
+        default:cout << "You are crawling. Next you can stand (0). Press 7 to finish\n";
+                cout << "Your choice: " << "\n";
+                cin >> choice;
+                state = (m_s)0;
                 break;
     }
+    }
+    cout << "Thanks for playing!\n";      
 
     return 0;
 }

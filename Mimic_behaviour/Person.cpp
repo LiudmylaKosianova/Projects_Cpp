4b948c6f2d31b617f2a96e70693f4bdc0aa87299
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-08-01 19:10:51 +0200

    mimic behaviour main

---
+++ Mimic_behaviour/Person.cpp
@@ -0,0 +1,18 @@
+#include "Person.h"
+
+Person::Person(){
+    
+}
+
+void Person::Crawl(){
+    cout << "\nCrawling\n";
+}
+void Person::Stand(){
+    cout << "\nStanding\n";
+}
+void Person::Walk(){
+    cout << "\nWalking\n";
+}
+void Person::Run(){
+    cout << "\nRunning\n";
+}
---
+++ Mimic_behaviour/main.cpp
@@ -0,0 +1,9 @@
+#include "Person.h"
+
+int main(){
+
+    Person *p1 = new Person();
+    p1->Crawl();    
+
+    return 0;
+}

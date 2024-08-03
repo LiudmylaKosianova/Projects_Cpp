49a597f19196d041dbc872e4452b634da8f29348
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-08-03 16:19:02 +0200

    Vehicle Manager works

--- Polymorphism/src/VehicleManager.cpp
+++ Polymorphism/src/VehicleManager.cpp
@@ -3,12 +3,28 @@
 
 VehicleManager::VehicleManager(int size) : maxVehicles(size){
     count = 0;
-    
+    vehicles = new Vehicle *[size];
 }
 bool VehicleManager::Add(Vehicle *v){
-    if(count == maxVehicles)return false;
-    else{
+    if(count < maxVehicles){
         vehicles[count] = v;
         count++;
+        return true;
+    }else{
+        return false;
     }
+}
+
+void VehicleManager::Display(){
+    for(int i = 0; i < count; i++){
+        vehicles[i]->Display();
+    }
+}
+
+int VehicleManager::GetCount(){
+    return count;
+}
+
+Vehicle** VehicleManager::GetList(){
+    return vehicles;
 }
--- Polymorphism/src/main.cpp
+++ Polymorphism/src/main.cpp
@@ -1,6 +1,7 @@
 
 #include "../headers/Boat.h"
 #include "../headers/Car.h"
+#include "../headers/VehicleManager.h"
 
 int main(){
 
@@ -11,15 +12,24 @@ int main(){
     // Car *myCar = new Car(151, 0);
     // myCar->Display();
 
-    Vehicle **myVehicles = new Vehicle*[5];
-    myVehicles[0] = new Car(1,35);
-    myVehicles[1] = new Boat(2, 55);
-    myVehicles[2] = new Vehicle(3, 30);
-    myVehicles[3] = new Car(4, 25);
-    myVehicles[4] = new Boat(5, 65);
-    for(int i = 0; i < 5; i++){
-        myVehicles[i]->Display();
-    }
+    // Vehicle **myVehicles = new Vehicle*[5];
+    // myVehicles[0] = new Car(1,35);
+    // myVehicles[1] = new Boat(2, 55);
+    // myVehicles[2] = new Vehicle(3, 30);
+    // myVehicles[3] = new Car(4, 25);
+    // myVehicles[4] = new Boat(5, 65);
+    // for(int i = 0; i < 5; i++){
+    //     myVehicles[i]->Display();
+    // }
+
+    VehicleManager myV(10);
+    myV.Add(new Car(1,35));
+    myV.Add(new Boat(2, 55));
+    myV.Add(new Vehicle(3,30));
+    myV.Add(new Car(4,25));
+    myV.Add(new Boat(5,65));
+    myV.Display();
+
     
     return 0;
 }

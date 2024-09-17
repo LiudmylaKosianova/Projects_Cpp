a92cd67df65b13444d09551a96c3d1063cb8a501
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-17 12:13:37 +0200

    class Satellite in progress

--- Space_mission_control/headers/Satellite.h
+++ Space_mission_control/headers/Satellite.h
@@ -4,5 +4,19 @@
 
 class Satellite : public Spacecraft
 {
-    
+    protected:
+    string orbitType; 
+    double operationalLifetime;// (in years)
+
+    public:
+    Satellite(string name, string date, string orbit, double lifetime);
+    ~Satellite();
+
+    void launch()override;
+    void completeMission()override;
+
+//     void deploySolarPanels(): Simulates the deployment of solar panels.
+// void adjustOrbit(): Adjusts the satellite's orbit.
+// void beginTransmission(): Starts transmitting data.
+
 };
--- Space_mission_control/src/Satellite.cpp
+++ Space_mission_control/src/Satellite.cpp
@@ -1,1 +1,29 @@
 #include "../headers/Satellite.h"
+
+Satellite::Satellite(string name, string launch, string orbit, double time) :
+Spacecraft(name, launch), orbitType(orbit), operationalLifetime(time)
+{
+    spacecraftType = "Satellite";
+    action = "is created";
+    Spacecraft::printAction;
+}
+
+Satellite::~Satellite()
+{
+    action = "is destroyed";
+    Spacecraft::printAction;
+}
+
+void Satellite::launch()
+{
+    action = "is launched";
+    Spacecraft::printAction;
+    status = "Launched";
+}
+
+void Satellite::completeMission()
+{
+    action = "is completed";
+    Spacecraft::printAction;
+    status = "Completed";
+}
